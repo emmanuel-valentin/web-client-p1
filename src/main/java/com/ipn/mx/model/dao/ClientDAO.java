64740a9ffaf7a008e748c5cc505d5abf9adc67d2
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2024-03-07 11:58:23 -0600

    get & getById has been added

--- src/main/java/com/ipn/mx/model/dao/ClientDAO.java
+++ src/main/java/com/ipn/mx/model/dao/ClientDAO.java
@@ -5,10 +5,14 @@
 package com.ipn.mx.model.dao;
 
 import com.ipn.mx.model.dto.ClientDTO;
+import com.ipn.mx.model.entities.Client;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,7 +21,12 @@ import java.util.logging.Logger;
  * @author emma
  */
 public class ClientDAO {
-    private final String SQL_INSERT = "INSERT INTO Client (nombre, paterno, materno, email) VALUES (?, ?, ?, ?)";
+
+    private static final String SQL__INSERT = "INSERT INTO Client (nombre, paterno, materno, email) VALUES (?, ?, ?, ?)";
+    private static final String SQL__UPDATE = "UPDATE Client SET nombre = ?, paterno = ?, materno = ?, email = ? WHERE id = ?";
+    private static final String SQL__DELETE = "DELETE FROM Client WHERE id = ?";
+    private static final String SQL__SELECT_ALL = "SELECT * FROM Client";
+    private static final String SQL__SELECT = "SELECT * FROM Client WHERE id = ?";
     private Connection conn;
 
     private void getConnection() {
@@ -39,7 +48,7 @@ public class ClientDAO {
         PreparedStatement ps = null;
 
         try {
-            ps = conn.prepareStatement(SQL_INSERT);
+            ps = conn.prepareStatement(SQL__INSERT);
             ps.setString(1, clientDTO.getEntity().getNombre());
             ps.setString(2, clientDTO.getEntity().getPaterno());
             ps.setString(3, clientDTO.getEntity().getMaterno());
@@ -54,5 +63,111 @@ public class ClientDAO {
                 conn.close();
             }
         }
+    }
+
+    public void update(ClientDTO clientDTO) throws SQLException {
+        getConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(SQL__UPDATE);
+            ps.setString(1, clientDTO.getEntity().getNombre());
+            ps.setString(2, clientDTO.getEntity().getPaterno());
+            ps.setString(3, clientDTO.getEntity().getMaterno());
+            ps.setString(4, clientDTO.getEntity().getEmail());
+
+            ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public void delete(int id) throws SQLException {
+        getConnection();
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(SQL__DELETE);
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public List<Client> get() throws SQLException {
+        getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List clients = new ArrayList<Client>();
+
+        try {
+            ps = conn.prepareStatement(SQL__SELECT_ALL);
+
+            if (ps.execute()) {
+                rs = ps.getResultSet();
+            }
+
+            while (rs.next()) {
+                Client clientResult = new Client();
+                clientResult.setId(rs.getInt(1));
+                clientResult.setNombre(rs.getString(2));
+                clientResult.setNombre(rs.getString(3));
+                clientResult.setNombre(rs.getString(4));
+                clientResult.setNombre(rs.getString(5));
+
+                clients.add(clientResult);
+            }
+
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        return clients;
+    }
+
+    public Client getById(int id) throws SQLException {
+        getConnection();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Client clientResult = null;
+
+        try {
+            ps = conn.prepareStatement(SQL__SELECT);
+            ps.setInt(0, id);
+
+            if (ps.execute()) {
+                rs = ps.getResultSet();
+            }
+
+            clientResult.setId(rs.getInt(1));
+            clientResult.setNombre(rs.getString(2));
+            clientResult.setNombre(rs.getString(3));
+            clientResult.setNombre(rs.getString(4));
+            clientResult.setNombre(rs.getString(5));
+
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        return clientResult;
     }
 }
